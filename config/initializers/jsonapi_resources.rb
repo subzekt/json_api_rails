3a43b5be7fba06066bb4630fa644dd5f63849080
subas45 <poudel.subas089@gmail.com>
2018-03-30 19:35:34 +0545

    added json_api resources, rspec, factory bot

---
+++ config/initializers/jsonapi_resources.rb
@@ -0,0 +1,6 @@
+JSONAPI.configure do |config|
+  # built in paginators are :none, :offset, :paged
+  config.default_paginator = :paged
+  config.default_page_size = 50
+  config.maximum_page_size = 1000
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      jsonapi_resources :contents
+      jsonapi_resources :pages
+    end
+  end
 end
